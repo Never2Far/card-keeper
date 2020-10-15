282b05adc23c163988990e54d49d8247b06d7cf8
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-10-15 08:54:20 -0500

    added methods to slugify and find username by slug

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ActiveRecord::Base
     has_many :sports, through: :cards
     has_many :teams, through: :cards
     has_many :players, through: :cards
+
+    def slug
+        slug = self.username.downcase.gsub(/[^a-zA-Z0-9. ]/, "").gsub(" ", "-")
+      end
+    
+      def self.find_by_slug(slug)
+        username = slug.gsub("-", " ")
+            self.all.each do |obj|
+                if obj.username.downcase.strip.gsub(/[^a-zA-Z0-9. ]/, "") == username
+                    return obj
+                end
+            end
+      end
 end
