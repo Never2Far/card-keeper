fb4f198415e31d7a3a01a878e1a0696a3118c820
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-10-15 09:08:15 -0500

    added root route and signup routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,5 +18,42 @@ class ApplicationController < Sinatra::Base
       User.find(session[:user_id])
     end
   end
+  
+    get '/' do
+      if self.logged_in?
+        redirect '/collections'
+      end
+  
+      erb :index
+      
+    end
+  
+  get '/signup' do
+    if self.logged_in?
+      redirect '/collections'
+    end
+  
+    erb :'users/signup'
+  end
+  
+  post '/signup' do
+    if params.any? {|k,v| v == ""}
+      redirect '/signup'
+    end
+  
+    user = User.new(
+      name: params[:name],
+      username: params[:username],
+      email: params[:email],
+      password: params[:password]
+    )
+    
+    if user.save
+      session[:user_id] = user.id
+      redirect '/collections'
+    else
+      redirect '/signup'
+    end
+  end
 
 end
