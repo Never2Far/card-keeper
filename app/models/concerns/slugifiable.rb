0f13511092915bee6f0fb3270d291bf4a188d34f
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-10-21 17:01:47 -0500

    separated instance & class methods to sep. modules

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,16 +1,18 @@
-module Slugifiable
-
-    def slug
-        slug = self.name.downcase.gsub(/[^a-zA-Z0-9. ]/, "").gsub(" ", "-")
+class Slugifiable
+    module InstanceMethods
+        def slug
+            slug = self.name.downcase.gsub(/[^a-zA-Z0-9. ]/, "").gsub(" ", "-")
+        end
     end
-    
-    def self.find_by_slug(slug)
+
+    module ClassMethods
+        def self.find_by_slug(slug)
             name = slug.gsub("-", " ")
             self.all.each do |obj|
                 if obj.name.downcase.strip.gsub(/[^a-zA-Z0-9. ]/, "") == name
                     return obj
                 end
             end
+        end
     end
-    
 end
