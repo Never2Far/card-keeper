c081bb00be1afece11a48ca7324f3ebcee4fb64f
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-10-21 17:05:17 -0500

    temp. added helper class method requirement

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,6 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
 
+require './app/models/concerns/slugifiable'
 require "rails_erd/diagram/graphviz"
 require_all 'app'
