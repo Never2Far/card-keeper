072719a5009c27178150569678eefc21e9b3613c
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-10-24 18:58:11 -0500

    added new and edit and delete routes

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,9 +1,92 @@
 class CardsController < ApplicationController
 
 
+get '/cards' do
+    erb :'cards/cards'
+end
+
+get '/cards/new' do
+    # @collection = Collection.find(session[:current_collection_id])
+    # @sport = @collection.sport
+    @user = User.find(session[:user_id])
+
+    erb :'cards/new'
+end
+
+post '/cards' do
+
+    @card = Card.create(
+        user_id: session[:user_id],
+        collection_id: params[:collection],
+        sport_id: Collection.find(params[:collection]).sport_id,
+        player_id: params[:player],
+        condition: params[:condition],
+        special_attribute: params[:special_attribute],
+        estimated_value: params[:estimated_value]
+    )
+
+    
+    
+    erb :'cards/new2'
+end
+
+post '/cards/new' do
+    @card = Card.find(params[:card])
+
+    contract = Contract.create(
+        player_id: @card.player_id,
+        team_id: params[:team],
+        position_id: params[:position]
+    )
+
+    @card.update(
+        contract_id: contract.id,
+        team_id: params[:team],
+        position_id: params[:position]
+    )
+
+   @card.save
+
+   redirect "/cards/#{@card.id}"
+
+
+end
+
 get '/cards/:id' do
     @card = Card.find(params[:id])
     erb :'cards/show'
+end
+
+get '/cards/:id/edit' do
+    
+    if !self.logged_in?
+        redirect '/index'
+    end
+    @card = Card.find(params[:id])
+    if self.current_user.id != @card.user_id
+        redirect "/cards/#{@card.id}"
+    end
+        erb :'cards/edit_card'
+    
+end
+
+patch '/cards/:id' do
+    @card = Card.find(params[:id])
+
+    if (params[:content] == "") || (params[:content] == nil)
+        redirect "/cards/#{@card.id}/edit"
+    end
+    
+    @card.update(content: params[:content])
+    redirect "/cards/#{@card.id}"
+end
+
+delete '/cards/:id' do
+    @card = Card.find(params[:id])
+    if self.logged_in? && (self.current_user.id == @card.user_id)
+        @card.destroy
+    end
+        redirect '/cards'
 end
 
 
