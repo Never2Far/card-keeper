245b57f4022531dca2e578523824e78eb5ea4400
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-10-24 18:57:15 -0500

    Added new and edit routes

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -3,7 +3,7 @@ class CollectionsController < ApplicationController
 
 
     get '/collections' do
-       if !logged_in?
+       if !self.logged_in?
         redirect '/index'
        end
 
@@ -11,9 +11,20 @@ class CollectionsController < ApplicationController
     end
 
 
+get '/collections/new' do
+    @user = User.find(session[:user_id])
+    erb :'collections/new'
+end
+
     get '/collections/:slug' do
         @collection = Collection.find_by_slug(params[:slug])
-        erb :'/collections/show'
+        # session[:current_collection_id] = @collection.id
+        erb :'collections/show'
+    end
+
+    get '/collections/:slug/edit' do
+        @collection = Collection.find_by_slug(params[:slug])
+        erb :'collections/edit'
     end
 
 
