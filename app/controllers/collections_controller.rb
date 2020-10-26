31cbabd60a322383db53717a323c57f053f06b42
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-10-25 20:25:08 -0500

    slug to using id's to reference collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -16,16 +16,66 @@ get '/collections/new' do
     erb :'collections/new'
 end
 
-    get '/collections/:slug' do
-        @collection = Collection.find_by_slug(params[:slug])
+post '/collections' do
+    @collection = Collection.create(
+        name: params[:name],
+        sport_id: params[:sport],
+        user_id: session[:user_id]
+    )
+
+    redirect "/collections/#{@collection.id}"
+end
+
+    get '/collections/:id' do
+        @collection = Collection.find(params[:id])
+        @value = @collection.getValue
         # session[:current_collection_id] = @collection.id
         erb :'collections/show'
     end
 
-    get '/collections/:slug/edit' do
-        @collection = Collection.find_by_slug(params[:slug])
+    get '/collections/:id/edit' do
+        @collection = Collection.find(params[:id])
         erb :'collections/edit'
     end
+
+    patch '/collections/:id' do
+        @collection = Collection.find(params[:id])
+
+        @collection.update(
+            name: params[:name],
+            sport_id: params[:sport]
+        )
+
+        
+        if params[:cards]
+        params[:cards].each do |card_id|
+            Card.find(card_id).destroy
+        end
+     end
+        redirect "/collections/#{@collection.id}"
+    end
+
+    delete '/collections/:id' do
+        @collection = Collection.find(params[:id])
+        if self.logged_in? && (self.current_user.id == @collection.user_id)
+
+            @collection.cards.each do |card|
+                card.destroy
+            end
+
+
+
+            @collection.destroy
+
+
+
+
+
+        end
+            redirect "/dashboard"
+    end
+
+
 
 
 
