13fdb38fa5de537cc45584de4b61959a14cb52b4
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-11-03 09:06:08 -0600

    removed typo

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,7 +9,7 @@ class PlayersController < ApplicationController
 
     get '/players/:slug' do
         @player = Player.find_by_slug(params[:slug])
-        erb :'/players/show'
+        erb :'players/show'
     end
 
 
