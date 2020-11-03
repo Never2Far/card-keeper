6afadec40b1d577d885dfd0de2e24fd4fa21618e
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-11-03 13:01:01 -0600

    added find by slug method to sport model

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,10 +1,19 @@
 class Sport < ActiveRecord::Base
     include Slugifiable::InstanceMethods
-    extend Slugifiable::ClassMethods
+    # extend Slugifiable::ClassMethods
     has_many :collections
     has_many :users, through: :collections
     has_many :teams
     has_many :players, through: :teams
     has_many :cards, through: :collections
     has_many :positions
+
+    def self.find_by_slug(slug)
+        name = slug.gsub("-", " ")
+        self.all.each do |obj|
+            if obj.name.downcase.strip.gsub(/[^a-zA-Z0-9. ]/, "") == name
+                return obj
+            end
+        end
+    end
 end
