5e9e64c1255da1368a6832f7cf24c3390a149851
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-11-03 14:18:05 -0600

    removed extraneous comments

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -18,15 +18,4 @@ class Collection < ActiveRecord::Base
         end
         total
     end
-
-
-
-    # def self.find_by_slug(slug)
-    #     name = slug.gsub("-", " ")
-    #     self.all.each do |obj|
-    #         if obj.name.downcase.strip.gsub(/[^a-zA-Z0-9. ]/, "") == name
-    #             return obj
-    #         end
-    #     end
-    # end
 end
